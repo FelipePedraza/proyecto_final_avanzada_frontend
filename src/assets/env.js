0a5f4d1ad7e105b2ac86e996d71ea6e561a8ed8f
FelipePedraza <davidf.pedrazab@uqvirtual.edu.co>
2026-04-04 13:21:32 -0500

    se corrigio la configuracion de variables de entorno para Azure

--- src/assets/env.js
+++ src/assets/env.js
@@ -1,6 +1,6 @@
 window.__env = {
-  apiUrl: process.env.API_URL,
-  wsUrl: process.env.WS_URL,
-  mapboxToken: process.env.MAPBOX_TOKEN,
-  stripePublicKey: process.env.STRIPE_PUBLIC_KEY,
+  apiUrl: '',
+  wsUrl: '',
+  mapboxToken: '',
+  stripePublicKey: '',
 };
